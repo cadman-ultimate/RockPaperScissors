b77dc9f51167b6f8b8f5310e725d97a0bdf34183
Cadman <81431317+cadman-ultimate@users.noreply.github.com>
2023-06-25 11:31:15 +0100

    CAD-004: Add Main method and JavaDoc

--- src/main/java/org/sabertech/Main.java
+++ src/main/java/org/sabertech/Main.java
@@ -1,37 +1,30 @@
 package org.sabertech;
 
-public class Main {
 
-    /**
-     * Rock Paper Scissors Game
-     *
-     * 1 = 100 iterations of the game
-     * 2 = Each Iteration:
-     * - One player always selects rock
-     * - The other player is a random selection
-     * - Store result
-     * 3 = At end of iterations present summary to user
-     */
+import org.sabertech.util.GameResult;
+
+import java.util.EnumMap;
+
+/**
+ * <b>Rock Paper Scissors Game</b>
+ * <p>
+ * 1 = 100 iterations of the game<br>
+ * 2 = Each Iteration:<br>
+ * - Player 1 always selects rock<br>
+ * - Player 2 is a random selection<br>
+ * 3 = At end of iterations present summary to user
+ */
+public class Main {
 
     public static void main(String[] args) {
 
-        /**
-         * In this method we want to do the following:
-         * 100 iterations of the game
-         * Get result of game: win lose or draw
-         * Store results:
-         *  - option 1: increment a count for each result => i += 1
-         *  - option 2: populate a list of every result then get count for each result
-         *  - option 3: populate map -> key is outcome, value is overall count of that result (maybe uses option 1 here for count)
-         * Get summary of results
-         * Print results to user
-         */
-
-        //Updated method
-        // 1 - Get the results
         GameProcessor processor = new GameProcessor();
+        EnumMap<GameResult, Integer> gameResult = processor.gameApplication();
 
-        // 2 - Print the summary to the user
+        System.out.println("Game results...");
+        System.out.println("Wins Player 1: " + gameResult.get(GameResult.LOSE));
+        System.out.println("Wins Player 2: " + gameResult.get(GameResult.WIN));
+        System.out.println("Draws: " + gameResult.get(GameResult.DRAW));
 
     }
 }
